f8512c86400e4b0a1bf20b920b3c0321aa28b468
sebanimm <gks32114221@gmail.com>
2022-09-03 23:15:16 +0900

    2022-09-03 | 정규식 추가, 날짜, 시간 표시 추가

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import "./App.css";
 import styled from "styled-components";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import GlobalFonts from "./fonts/fonts";
+import "./App.css";
 
 const Container = styled.div`
+  height: fit-content;
   background-color: gray;
   margin: 0;
   padding: 0;
 `;
 
 const TodaysDate = styled.h1`
+  padding: 15vh 0;
   margin: 0;
   height: 10vh;
   text-align: center;
 `;
 
-const BssmBob = styled.div`
+const Bob = styled.div`
   width: 300px;
-  height: 270px;
-  background: #ecfae7;
+  height: 300px;
+  background: #d8e9d8;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.4);
-  border-radius: 0 0 10px 10px;
+  border-radius: 10px;
   position: relative;
   text-align: center;
 `;
@@ -30,7 +33,7 @@ const BobHeader = styled.div`
   height: 30px;
   width: 300px;
   position: absolute;
-  top: -30px;
+  top: 0px;
   border-radius: 10px 10px 0px 0px;
 `;
 
@@ -54,30 +57,58 @@ const ThirdCircle = styled(FirstCircle)`
   margin-left: 50px;
 `;
 
+const BobTime = styled.p`
+  font-size: 30px;
+  font-family: "nanum";
+  margin-bottom: 2vh;
+`;
+
+const BobInfo = styled.div`
+  display: flex;
+  white-space: pre-wrap;
+  line-height: 150%;
+  font-size: 20px;
+  font-family: "nanum";
+  height: 300px;
+  flex-direction: column;
+  justify-content: center;
+`;
+
 function App() {
   const date = new Date();
+  const week = ["일", "월", "화", "수", "목", "금", "토"];
   const year = String(date.getFullYear());
   const month = String(date.getMonth() + 1);
   const day = String(date.getDate());
+  const dayOfTheWeek = week[new Date(year, month - 1, day).getDay()];
+  console.log(new Date(year, month - 1, day));
+  console.log(date);
   const currentDate = `${year}${month.padStart(2, "0")}${day.padStart(2, "0")}`;
-  const [breakfastInfo, setBreakfastInfo] = useState([]);
-  const [lunchInfo, setLunchInfo] = useState([]);
-  const [dinnerInfo, setDinnerInfo] = useState([]);
 
-  /** 급식 정보가 없을 경우 기본값 출력 -> json파일이 급식 정보가 없을 때 경로가 바뀌는 것 때문에 못함*/
-  /** 괄호 제거 -> 정규식 만들고 설정까지 했는데 먹지를 않음*/
+  const [breakfastInfo, setBreakfastInfo] = useState("급식 정보가 없습니다.");
+  const [lunchInfo, setLunchInfo] = useState("급식 정보가 없습니다.");
+  const [dinnerInfo, setDinnerInfo] = useState("급식 정보가 없습니다.");
 
   const URL = `https://open.neis.go.kr/hub/mealServiceDietInfo?&Type=json&pIndex=1&pSize=10&ATPT_OFCDC_SC_CODE=C10&SD_SCHUL_CODE=7150658&MLSV_YMD=${currentDate}`;
+  const firstRegex = /<br\/>|\([^\)]*\)/g;
+  const secondRegex = /\s{2}/g;
   const getBobInfo = () => {
     axios.get(URL).then((res) => {
-      const breakfast =
-        res.data.mealServiceDietInfo[1].row[0].DDISH_NM.replaceAll("<br/>");
+      const data = res.data.mealServiceDietInfo[1].row;
+      const breakfast = data[0].DDISH_NM.replace(firstRegex, "").replace(
+        secondRegex,
+        "\n"
+      );
       setBreakfastInfo(breakfast);
-      const lunch =
-        res.data.mealServiceDietInfo[1].row[1].DDISH_NM.replaceAll("<br/>");
+      const lunch = data[1].DDISH_NM.replace(firstRegex, "").replace(
+        secondRegex,
+        "\n"
+      );
       setLunchInfo(lunch);
-      const dinner =
-        res.data.mealServiceDietInfo[1].row[2].DDISH_NM.replaceAll("<br/>");
+      const dinner = data[2].DDISH_NM.replace(firstRegex, "").replace(
+        secondRegex,
+        "\n"
+      );
       setDinnerInfo(dinner);
     });
   };
@@ -90,34 +121,44 @@ function App() {
 
   return (
     <Container>
+      <GlobalFonts />
       <TodaysDate>
-        {month}월 {day}일
+        {month}월 {day}일 {dayOfTheWeek}요일
       </TodaysDate>
       <div className="App">
-        <BssmBob>
-          <BobHeader>
-            <FirstCircle />
-            <SecondCircle />
-            <ThirdCircle />
-          </BobHeader>
-          <p>{breakfastInfo}</p>
-        </BssmBob>
-        <BssmBob>
-          <BobHeader>
-            <FirstCircle />
-            <SecondCircle />
-            <ThirdCircle />
-          </BobHeader>
-          <p>{lunchInfo}</p>
-        </BssmBob>
-        <BssmBob>
-          <BobHeader>
-            <FirstCircle />
-            <SecondCircle />
-            <ThirdCircle />
-          </BobHeader>
-          <p>{dinnerInfo}</p>
-        </BssmBob>
+        <div>
+          <BobTime>아침</BobTime>
+          <Bob>
+            <BobHeader>
+              <FirstCircle />
+              <SecondCircle />
+              <ThirdCircle />
+            </BobHeader>
+            <BobInfo>{breakfastInfo}</BobInfo>
+          </Bob>
+        </div>
+        <div>
+          <BobTime>점심</BobTime>
+          <Bob>
+            <BobHeader>
+              <FirstCircle />
+              <SecondCircle />
+              <ThirdCircle />
+            </BobHeader>
+            <BobInfo>{lunchInfo}</BobInfo>
+          </Bob>
+        </div>
+        <div>
+          <BobTime>저녁</BobTime>
+          <Bob>
+            <BobHeader>
+              <FirstCircle />
+              <SecondCircle />
+              <ThirdCircle />
+            </BobHeader>
+            <BobInfo>{dinnerInfo}</BobInfo>
+          </Bob>
+        </div>
       </div>
     </Container>
   );
---
+++ src/fonts/fonts.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  @font-face {
+      font-family: 'nanum';
+      src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/NanumSquareRound.woff') format('woff');
+      font-weight: normal;
+      font-style: normal;
+  }
+`;
