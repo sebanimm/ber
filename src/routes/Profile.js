33c9195504dfa158946447ebfefb0cf222cb9c66
sebanimm <gks32114221@gmail.com>
2022-10-10 16:22:37 +0900

    2022-10-09

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -3,16 +3,12 @@ import styled from "styled-components";
 import Container from "../styles/Container";
 
 const User = styled.div`
-	width: 1000px;
-	height: 320px;
-	margin: 0 auto;
 	display: flex;
 	flex-direction: column;
 	justify-content: space-around;
 `;
 
 const UserProfile = styled.div`
-	width: 100%;
 	height: 250px;
 	background: #98adc7;
 	box-shadow: 3px 4px 8px 3px rgba(0, 0, 0, 0.25);
@@ -24,21 +20,58 @@ const ProfileEditBtn = styled.div`
 	height: 35px;
 	border: 1px solid black;
 	border-radius: 20px;
-	margin-left: auto;
+	margin: 0 3% 0 auto;
 	font-size: 12px;
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	margin-right: 3%;
+`;
+
+const Posts = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	min-height: calc(100vh - 500px);
+	flex-direction: column;
+`;
+
+const Wrapper = styled.div`
+	padding-top: 30px;
+	max-width: 1000px;
+	margin: 0 auto;
+`;
+
+const PostStatus = styled.div`
+	font-size: 34px;
+	text-align: center;
+	color: #505050;
+	width: fit-content;
+	margin: 0 auto;
+`;
+
+const SortingBtn = styled(ProfileEditBtn)`
+	width: 100px;
+	margin-top: 20px;
+	margin-bottom: 20px;
 `;
 
 const Profile = () => {
 	return (
 		<Container>
-			<User>
-				<ProfileEditBtn>프로필 편집</ProfileEditBtn>
-				<UserProfile></UserProfile>
-			</User>
+			<Wrapper>
+				<User>
+					<ProfileEditBtn>프로필 편집</ProfileEditBtn>
+					<div style={{ marginBottom: "30px" }} />
+					<UserProfile></UserProfile>
+				</User>
+				<SortingBtn>좋아요순</SortingBtn>
+				<Posts>
+					<PostStatus>
+						<p>게시글이 없네요.</p>
+						<p>친구들과 멋진 글을 공유해보세요!</p>
+					</PostStatus>
+				</Posts>
+			</Wrapper>
 		</Container>
 	);
 };
--- src/styles/VerticalLine.js
+++ src/styles/VerticalLine.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const VerticalLine = styled.div`
 	border: 1.2px solid #6a84a4;
 	width: 0;
-	height: 460px;
+	height: 60vh;
 	border-radius: 5px;
 	top: 50%;
 	background-color: #6a84a4;
